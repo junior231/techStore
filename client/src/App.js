9ffc9dc6eb84f2dee830c322938dd4ec50bac835
junior231 <ilocollins231@gmail.com>
2023-02-12 22:08:49 -0500

    Create Navbar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <ChakraProvider>
+      <BrowserRouter>
+        <Navbar />
+        <main>{/* <Routes></Routes> */}</main>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
 
 export default App;
