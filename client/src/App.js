050cafc8731e02581eff5a5b8b1cccf5b36616fa
junior231 <ilocollins231@gmail.com>
2023-02-13 21:25:52 -0500

    Products Screen and Product Card added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProductsScreen from "./screens/ProductsScreen";
 
 function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
         <Navbar />
-        <main>{/* <Routes></Routes> */}</main>
+        <main>
+          <Routes>
+            <Route element={<ProductsScreen />} path="/products" />
+          </Routes>
+        </main>
       </BrowserRouter>
     </ChakraProvider>
   );
