d7a8933c4599343c32640f921e0af40486b888a4
junior231 <ilocollins231@gmail.com>
2023-02-26 21:48:47 -0500

    Create Single Product Screen.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CartScreen from "./screens/CartScreen";
+import ProductScreen from "./screens/ProductScreen";
 import ProductsScreen from "./screens/ProductsScreen";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <main>
           <Routes>
             <Route element={<ProductsScreen />} path="/products" />
+            <Route element={<ProductScreen />} path="/product/:id" />
             <Route element={<CartScreen />} path="/cart" />
           </Routes>
         </main>
--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,13 +1,36 @@
 import axios from "axios";
-import { setProducts, setError, setLoading } from "../slices/products";
+import {
+  setProducts,
+  setError,
+  setLoading,
+  setProduct,
+} from "../slices/products";
 
-const API_ENDPOINT = "/api/products";
+const GET_PRODUCTS = "/api/products";
 
 export const getProducts = () => async (dispatch) => {
   dispatch(setLoading(true));
   try {
-    const { data } = await axios.get(API_ENDPOINT);
+    const { data } = await axios.get(GET_PRODUCTS);
     dispatch(setProducts(data));
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : "An unexpected error has occured. Please try again later."
+      )
+    );
+  }
+};
+
+export const getProduct = (id) => async (dispatch) => {
+  setLoading(true);
+  try {
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch(setProduct(data));
   } catch (error) {
     dispatch(
       setError(
