4eb8886e3be727c06d3d9e64d9be0136096cb20a
junior231 <ilocollins231@gmail.com>
2023-02-27 21:17:15 -0500

    Add Footer and Landing Screen.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import CartScreen from "./screens/CartScreen";
 import ProductScreen from "./screens/ProductScreen";
 import ProductsScreen from "./screens/ProductsScreen";
+import Footer from "./components/Footer";
+import LandingScreen from "./screens/LandingScreen";
 
 function App() {
   return (
@@ -12,11 +14,14 @@ function App() {
         <Navbar />
         <main>
           <Routes>
+            <Route element={<LandingScreen />} path="/" />
             <Route element={<ProductsScreen />} path="/products" />
             <Route element={<ProductScreen />} path="/product/:id" />
             <Route element={<CartScreen />} path="/cart" />
           </Routes>
         </main>
+
+        <Footer />
       </BrowserRouter>
     </ChakraProvider>
   );
